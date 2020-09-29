971d63aa1f7b7790650c1a4486ef42a4038bbbbb
saladinjake <juwavictor@gmail.com>
2020-09-29 20:31:27 +0100

    Imported modules and other libs needed in the app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './core/auth/login/login.component';
+import { RegisterComponent } from './core/auth/register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import {
+  FormsModule,
+  ReactiveFormsModule
+
+} from '@angular/forms';
+import { ToastrModule } from 'ng6-toastr-notifications';
+
+import { MaterialModule } from './shared/material.module';
+
+
+import { LoginComponent } from './core/auth/login/login.component';
+import { ToasterService } from './shared/services/toaster.service';
+import { RegisterComponent } from './core/auth/register/register.component';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MaterialModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    MatSnackBarModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  entryComponents: [
+    LoginComponent,
+  ],
+  providers: [ToasterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
